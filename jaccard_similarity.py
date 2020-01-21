4bb84cd7b8c923f90e513b22aff8da24eb2a6fa3
sharif_42 <md.shariful@circlefintech.com>
2020-01-21 18:05:43 +0600

    jaccard similarity progressing

--- jaccard_similarity.py
+++ jaccard_similarity.py
@@ -1,22 +1,56 @@
-def jaccard_similarity(lst1, lst2):
-    intersection = 0
-    union = 0
-    for a, b in zip(lst1, lst2):
-        if a == b == 1:
-            intersection += 1
-        if a == 1 or b == 1:
-            union += 1
-    print(intersection, union)
-    return intersection / union
+class JaccardSimilarity:
+
+    @staticmethod
+    def get_transpose_matrix(lst):
+        transpose_matrix = [[row[i] for row in lst] for i in range(len(lst[0]))]
+        return transpose_matrix
+
+    @staticmethod
+    def jaccard_similarity(lst1, lst2):
+        intersection = 0
+        union = 0
+        for a, b in zip(lst1, lst2):
+            if a == b == 1:
+                intersection += 1
+            if a == 1 or b == 1:
+                union += 1
+        print(intersection, union)
+        return intersection / union
 
 
 if __name__ == "__main__":
-    lst = [
-        [1, 0, 0, 1, 0, 1, 1],
-        [0, 1, 1, 0, 0, 1, 1],
-        [1, 1, 0, 1, 0, 1, 0],
-        [1, 1, 0, 1, 1, 0, 0]
-    ]
-    for i in range(3):
-        print("jaccard similirity between column {} and {} is :{}".
-              format(i + 1, i + 2, jaccard_similarity(lst[i], lst[i + 1])))
+    column = 4
+    columns = [i for i in range(column)]
+    print(columns)
+
+    lst = [[1, 0, 1, 1],
+           [0, 1, 1, 1],
+           [0, 1, 0, 0],
+           [1, 0, 1, 1],
+           [0, 0, 0, 1],
+           [1, 1, 1, 0],
+           [1, 1, 0, 0]
+           ]
+    print(lst[0])
+    transpose_matrix = JaccardSimilarity.get_transpose_matrix(lst)
+
+    # for i in range(3):
+    #     print("jaccard similirity between column {} and {} is :{}".
+    #           format(i + 1, i + 2, jaccard_similarity(lst[i], lst[i + 1])))
+
+    # rows, columns = list(map(int, input().split()))  # input no. of row and column
+    # lst = []
+    # for i in range(rows):
+    #     a = list(map(int, input().split()))
+    #     lst.append(a)
+
+    """ dummy inputs
+    7 4
+    1 0 1 1
+    0 1 1 1
+    0 1 0 0
+    1 0 1 1
+    0 0 0 1
+    1 1 1 0
+    1 1 0 0
+    """
