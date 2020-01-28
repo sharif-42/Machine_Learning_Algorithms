faee70d4108cc46aa8a3a8c79baad8d5bde0b47b
sharif-42 <sharifulcsehstu@gmail.com>
2020-01-29 00:00:12 +0600

    jaccard similarity updated

--- Algorithms/jaccard_similarity.py
+++ Algorithms/jaccard_similarity.py
@@ -1,24 +1,30 @@
 class JaccardSimilarity:
+    # A Class for searching Jaccard Similarity
 
     @staticmethod
     def get_transpose_matrix(lst):
+        # This Function take a matrix as input and return the transpose matrix of the input matrix
         transpose_matrix = [[row[i] for row in lst] for i in range(1, len(lst[0]))]
         return transpose_matrix
 
     @staticmethod
     def similarity_calc(lst1, lst2):
+        """
+        This is the actual function to calculate jaccard similarity.
+        take two column as input and calculate jaccard similarity between the two column
+        """
         intersection = 0
         union = 0
         for a, b in zip(lst1, lst2):
-            if a == b == 1:
+            if a == b == 1:           # calculate intersection between two column
                 intersection += 1
-            if a == 1 or b == 1:
+            if a == 1 or b == 1:      # calculate union between two column
                 union += 1
-        return intersection / union
+        return intersection / union   # return jaccard similarity between two column
 
 
 if __name__ == "__main__":
-    matrix = [['SHINGLES', 'D1', 'D2', 'D3'],
+    matrix = [['SHINGLES', 'D1', 'D2', 'D3'],   # this is our matrix
            ['This', 1, 1, 0],
            ['his_', 1, 1, 0],
            ['is_i', 1, 1, 0],
@@ -51,12 +57,12 @@ if __name__ == "__main__":
            ['t_is', 0, 0, 1],
            ]
     print("Input Matrix:\n")
-    for row in matrix:
-      print(row)
-    transpose_matrix = JaccardSimilarity.get_transpose_matrix(matrix[1:])
-    needed_list = [(0, 1), (0, 2), (1, 2)]
+    for row in matrix:       # show the input matrix
+        print(row)
+    transpose_matrix = JaccardSimilarity.get_transpose_matrix(matrix[1:])  # transpose the matrix
+    needed_list = [(0, 1), (0, 2), (1, 2)]    # combination of column for which we calculate the jaccard similarity
 
     print("\nOutput:")
-    for i in needed_list:
+    for i in needed_list:    # Calculation for jaccard similarity
         print("jaccard similirity {D%d , D%d} = %0.2f" %
               (i[0]+1, i[1]+1, JaccardSimilarity.similarity_calc(transpose_matrix[i[0]], transpose_matrix[i[1]])))
