bf0a07ed39626728405a7d2b30bd998e4bc6e301
sharif_42 <md.shariful@circlefintech.com>
2020-02-16 11:42:59 +0600

    evaluation matrix progressing

---
+++ Algorithms/evaluation_metrics.py
@@ -0,0 +1,10 @@
+class EvaluationMetrics:
+    def __init__(self, actual, predicted):
+        self.actual = actual
+        self.predicted = predicted
+
+    def jaccard_similarity(self):
+        pass
+
+    def confusion_matrix(self):
+        pass
